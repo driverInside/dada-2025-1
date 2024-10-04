9c950033f7bfd0fe161d8f03b617f43e7f3471a8
Hiram E. Pérez <hiram.eps@gmail.com>
2024-10-04 08:18:05 -0600

    update linked list

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # dada-2025-1
 Design and analysis class codes.
+
+
+## Python
+
+### Instalar pip
+
+En entornos linux:
+
+```bash
+$ python -m ensurepip --upgrade
+```
+
+Linux Debian
+
+```bash
+$ sudo apt-get install python3-pip
+```
+
+Puedes seguir las instrucciones del [Sitio Oficial](https://pip.pypa.io/en/stable/installation/).
+
+
+### Instalar Matplotlib
+
+Para instalar esta librería primero se debe instalar `pip`. Para verificar si se tiene instalado, se puede ejecutar:
+
+```bash
+$ python3 -m pip --version
+```
+
+Para instalar matplodlib:
+```bash
+$ python -m pip install -U matplotlib
+```
+
+O bien, en Linux Debian:
+
+```bash
+$ sudo apt-get install python3-matplotlib
+```
+
+## Node
+
+JSdom
+
+d3.js
+
+
--- merge/merge.js
+++ merge/merge.js
@@ -1,7 +1,7 @@
 class Node {
-  constructor (data) {
-    this.data = data;
-    this.next = null;
+  constructor (value) {
+    this.value = value;
+    this.next = null
   }
 }
 
@@ -12,43 +12,106 @@ class LinkedList {
     this.tail = null;
   }
 
-  /**
-   * append
-   * @description - Agrega un nodo al final de la lista.
-   * @param {Node} node - El nodo a agregar 
-   */
-  append(node) {
-
-  }
-
-  /**
-   * removeHead
-   * @description - Elimina el nodo head de la lista y lo regresa.
-   * @returns {Node} - El nodo que se quita de la lista.
-   */
-  removeHead() {
-
-  }
-
-  /**
-   * getSize
-   * @returns {number} - El tamaño de la lista.
-   */
   getSize() {
     return this.size;
-  } 
+  }
+
+  append(node) {
+    if (this.size === 0) {
+      // la lista está vacía.
+
+      this.head = node;
+      this.tail = node;
+      this.size++;
+    } else {
+      // tarea
+    }
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  toString () {
+    // tarea
+  }
 }
 
-const nodeOne = new Node(2);
-const nodeTwo = new Node(3);
-const nodeThree = new Node(5);
-const nodeFour = new Node(8);
+const nodeTwo = new Node(2);
+const nodeFive = new Node(5);
 
-const listA = new LinkedList();
-listA.append(nodeOne);
-listA.append(nodeTwo);
-listA.append(nodeThree);
-listA.append(nodeFour);
 
-console.log(listA.getSize());
+const linkedList = new LinkedList();
+
+console.log(linkedList.isEmpty()); // true
+
+linkedList.append(nodeTwo);
+linkedList.append(nodeFive);
+
+console.log(linkedList.isEmpty()); // false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class LinkedList {
+//   constructor () {
+//     this.size = 0;
+//     this.head = null;
+//     this.tail = null;
+//   }
+
+//   /**
+//    * append
+//    * @description - Agrega un nodo al final de la lista.
+//    * @param {Node} node - El nodo a agregar 
+//    */
+//   append(node) {
+
+//   }
+
+//   /**
+//    * removeHead
+//    * @description - Elimina el nodo head de la lista y lo regresa.
+//    * @returns {Node} - El nodo que se quita de la lista.
+//    */
+//   removeHead() {
+
+//   }
+
+//   /**
+//    * getSize
+//    * @returns {number} - El tamaño de la lista.
+//    */
+//   getSize() {
+//     return this.size;
+//   } 
+// }
+
+// const nodeOne = new Node(2);
+// const nodeTwo = new Node(3);
+// const nodeThree = new Node(5);
+// const nodeFour = new Node(8);
+
+// const listA = new LinkedList();
+// listA.append(nodeOne);
+// listA.append(nodeTwo);
+// listA.append(nodeThree);
+// listA.append(nodeFour);
+
+// console.log(listA.getSize());
 
